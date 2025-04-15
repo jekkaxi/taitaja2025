bb9478506e2c6e1f45e7d0baf1b27e96f08f3baa
jekkaxi <jessica.kautto@student.kpedu.fi>
2025-04-15 10:04:18 +0300

    Update startgame.php

--- startgame.php
+++ startgame.php
@@ -1,5 +1,15 @@
 <?php
 include "connect.php";
+$teacher_id = $_POST['opettaja'];
+$category_id = $_POST['kategoria'];
+$number_of_wuestions = $_POST['kysymykset'];
+$sql = "SELECT * FROM questions WHERE category_id = $category_id AND teacher_id = $teacher_id ORDER BY RAND() LIMIT $number_of_wuestions";
+$result = $conn->query($ql);
+
+$wuestions = [];
+while ($row = $result->fetch_assoc()) {
+    $wuestions[] = $row;
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,46 +33,69 @@ include "connect.php";
                 <a href="login.html">Kirjaudu sisään</a>
         </header>
 
-        <?php
-        //tulostaa PHP:lla mitä käyttäjä on valinuut viime sivulla
-        //echo $_POST['opettaja']; echo "<br>";
-        //echo $_POST['kategoria']; echo "<br>";
-        //echo $_POST['kysymykset'];
-        ?>
-        <div class="keskita">
-            <?php
-            $kysymysnr = $_POST['kysymysnr'];
-            ?>
-            <p>
-                kysymys <?php echo $kysymysnr;?>/10
-            </p>
-            <p>
-                pisteet: 3
-            </p>
+        <center>
+            <p id="kysymykset">Kysymys <span id="currentQuestion">1</span>/<?php echo $number_of_wuestions-1;?></p>
+            <p>Pisteet <span id="score">0</span></p>
+        </center>
+        <center>
+            <button id="nedt-button" onclick="nextQuestion()">Seuraava kysymys</button>
+        </center>
+        <center>
+            <input hidden type="text" name="highscore" id="highscore" placeholder="Kirjoita nimesi tuloslistaan">
+        </center><br><br>
 
-            <h1>Mitä CSS tekee verkkosivulla?</h1>
-            <table>
-                <tr>
-                    <td><input type="radio" name="vaihtoehto1" id="vastaus1"></td>
-                    <td><p>Määrittää ulkoasun</p></td>
+        <script>
+            const wuestion = <?php echo json_encode($wuestions); ?>;
+            let currentQuestion = 1;
+            let score = 0;
 
-                    <td><input type="radio" name="vaihtoehto2" id="vastaus2"></td>
-                    <td><p>Tallentaa tietoja</p></td>
-                </tr>
-                <tr>
-                    <td><input type="radio" name="vaihtoehto3" id="vastaus3"></td>
-                    <td><p>Lisää toiminnallisuutta</p></td>
+            function showQuestion(index) {
+                const question = question[index];
+                document.getElementById('wuestion-container').innerHTML = `
+                    <h1 class"pelih1">${question.wuestion}</h1>
+                    <table>
+                    <tr>
+                        <td><input type="radio" name="answer" value="A"></td><p> ${wuestion.option_a}</p></td>
+                        <td><input type="radio" name="answer" value="B"></td><p> ${wuestion.option_b}</p></td>
+                    </tr>
+                    <tr>
+                        <td><input type="radio" name="answer" value="C"></td><p> ${wuestion.option_c}</p></td>
+                        <td><input type="radio" name="answer" value="BD"></td><p> ${wuestion.option_d}</p></td>
+                    </tr>
+                    </table></center>
+                    `;
+            }
 
-                    <td><input type="radio" name="vaihtoehto4" id="vastaus4"></td>
-                    <td><p>Hoitaa tietoturvaa</p></td>
-                </tr><br><br>
-            </table>
-            <a href="#"><button>Vastaa</button></a><br><br>
-        </div>
-        <footer>
-            <h2>
-                Taitaja2025 -semifinaali
-            </h2>
+            function nextQuestion() {
+                const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+                if (selectedAnswer) {
+                    const correctAnswer = questions[currentQuestion].correct_option;
+                    if (selectedAnswer.value === correctAnswer) {
+                        score++;
+                        documetn.getElementById('score').textContent 0 score;
+                    }
+                }
+
+                currentQuestion++;
+                document.getElementById('currentQuestion').textContent = currentQuestion;
+
+                if (currentQuestion < wuestion.lenght) {
+                    showQuestion(currentQuestion);
+                } else {
+                    document.getElementById('otsikko-container').innerHTML ='<center><h2>Tuloksesi</2></center>';
+                    document.getElementById('question-container').innerHTML =`<center><p>Pisteet: ${score}/<?php echo $number_of_questions-1;?></p></center>`;
+                    document.getElementById('next-button').style.display ='none';
+                    document.getElementById('kysymykset').style.display ='none';
+                    document.getElementById('highscore').style.display ='block';
+                }
+            }
+
+            showQuestion(currentQuestion);
+
+            document.querSelector('.answer-container').addEventListener('change', function() {
+                document.getElementById('next-button').style.display = 'inline';
+            });
+            </script>
             <p>
                 Jessica Kautto | Keski-Pohjanmaan ammattiopisto
             </p>
